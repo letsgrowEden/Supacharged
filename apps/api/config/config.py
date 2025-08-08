43fc487544e1c7c2d8a34346f9cdad2e519d9df1
letsgrowEden <letsgrow.eden@gmail.com>
2025-08-08 10:33:28 +0200

    Add gemini api key

--- apps/api/config/config.py
+++ apps/api/config/config.py
@@ -1,11 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     # This tells Pydantic to load variables from a .env file
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     SUPABASE_URL: str
     SUPABASE_KEY: str
+    GEMINI_API_KEY: str
+
 
 # Create a single, reusable instance of the settings
 settings = Settings()
